3ab52ed54a8e95d201408af701d8f954bd7f23ce
Mino <skyhan1106@gmail.com>
2015-11-28 20:39:29 +0900

    modify_151128

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'purchase/save_data'
 
   get 'index/home'
+  get 'index/main'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
